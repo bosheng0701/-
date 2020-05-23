6cc878a160271eb7e827543ef9c76c62b26c8a12
bosheng0701 <sgps8871@gmail.com>
2020-05-23 16:37:56 +0800

    connectsuccess

--- mysqlconnect.py
+++ mysqlconnect.py
@@ -1,17 +1,22 @@
 import pymysql 
 
 #打開資料庫連接，設定連接帳號root，密碼password，資料庫student
-db= pymysql.connect("localhost","root","password","student")
+db= pymysql.connect("127.0.0.1","root1","password","root1")
 
 #使用cursor()方法得到操作指標
 cursor=db.cursor()
 
 #執行SQL語句
-cursor.execute("SELECT VERSION()")
+cursor.execute("SELECT id,password FROM `user` ")
 
 #使用fetchone()得到資料
 data =cursor.fetchone()
+print(data)
 
-print("Database version: %s"%data)
+
+if(data[0]==1 and data[1]==123123):
+    print("success login!!!")
+else:
+    print("ID or password is wrong~")
 
 db.close()
